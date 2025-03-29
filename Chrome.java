1fe5416785200899da930f79e02b2db080db1430
Sarankumar589 <sarankumar589uk@gmail.com>
2025-03-29 10:45:32 +0000

    Add files via upload
    Selenium Assignment 1

---
+++ Chrome.java
@@ -0,0 +1,32 @@
+package week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		
+//Float
+		float version=91.0f;
+		System.out.println(version);
+
+//String
+		String developer="Google";
+		System.out.println(developer);
+		
+//Boolean
+		boolean isBeta=false;
+System.out.println(isBeta);
+
+//Int
+		int releaseYear= 2008;
+		System.out.println(releaseYear);
+		
+//Char
+		char shortcutKey= 'c';
+		
+		System.out.println(shortcutKey);
+		
+	}
+}
+
+
+
